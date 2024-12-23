1b77e3d0fcb877d850c99620fede06ba34ff9c00
muhammadfarras <maruffarras@gmail.com>
2024-12-23 16:31:44 +0700

    awalan

---
+++ src/test/java/com/farras/LearnGraphql/LearnGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farras.LearnGraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
